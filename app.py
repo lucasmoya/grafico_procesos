72eb571f69827180e1f3f1841c2108f0d6685c00
Lucas Moya Jung <lucas.moya_externos@abastible.cl>
2026-01-29 17:50:10 -0300

    ancho

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ try:
     hoy = pd.to_datetime(datetime.now().date())
     linea_hoy = alt.Chart(pd.DataFrame({'hoy': [hoy]})).mark_rule(color='blue', strokeDash=[5, 5]).encode(x='hoy:T')
     
-    st.altair_chart((bars_timeline + linea_hoy).properties(height=400).interactive(), use_container_width=True)
+    st.altair_chart((bars_timeline + linea_hoy).properties(height=300).interactive(), use_container_width=True)
 
     # --- GRÁFICO DE AVANCE (BARRAS DE UN SOLO COLOR) ---
     st.divider()
