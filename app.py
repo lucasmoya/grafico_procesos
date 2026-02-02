b1c2bf02885b55644d0559f71dd62a7c6fa210fa
Lucas Moya Jung <lucas.moya_externos@abastible.cl>
2026-02-02 14:04:48 -0300

    asdsad

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ try:
             header=alt.Header(
                 labelAngle=0, 
                 labelAlign='left',
-                labelLimit=150  # <--- AJUSTA ESTE VALOR: Define cuántos píxeles ocupará el texto antes de abreviarse
+                labelLimit=100  # <--- AJUSTA ESTE VALOR: Define cuántos píxeles ocupará el texto antes de abreviarse
             )
         )
     ).configure_view(
